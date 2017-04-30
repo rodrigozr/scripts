ae03e0794dbeb5cb22cee9051f02bb4b99efa625
Rodrigo Rosauro <rrosauro@Rodrigos-iMac.local>
2017-04-30 18:47:08 -0300

    compress-mp4.sh v1.1

--- compress-mp4.sh
+++ compress-mp4.sh
@@ -26,30 +26,47 @@ shopt -s nocaseglob
 for f in $INPUT_FOLDER/*.MP4
 do
     FNAME=$(basename $f)
-	echo "Processing file: $FNAME ..."
-    # Get the file bitrate
-    bitrate=$($FFMPEG -i $f 2>&1 | grep -o "bitrate: \d*" | cut -b10-)
+    echo "Processing file: $FNAME ..."
+    # Get the file bitrate and size
+    bitrate=$($FFMPEG -i $f -nostdin 2>&1 | grep -o "bitrate: \d*" | cut -b10-)
+    size=$(du -h $f | cut -f1)
     # Check if the video is rotated
     rotated=NO
     resolution=1280x720
     if $FFMPEG -i $f 2>&1 | grep rotate > /dev/null; then
         rotated=YES
-        resolution=720X1280
+        resolution=720x1280
     fi
-    echo "    CURRENT BITRATE: $bitrate"
-    echo "    ROTATED: $rotated"
+
+    # Print source file information
+    echo "  CURRENT BITRATE: $bitrate"
+    echo "  CURRENT SIZE   : $size"
+    echo "  ROTATED        : $rotated"
     if [[ $bitrate -lt 6000 ]]; then
-        echo "    (IGNORING FILE - ALREADY AT A LOW BITRATE)"
+        echo "  (IGNORING FILE - ALREADY AT A LOW BITRATE)"
+        echo ""
         continue
     fi
-    echo "    Compressing..."
+
+    # Transcode the file
+    echo "  Compressing..."
     out=$OUTPUT_FOLDER/$FNAME
-    $FFMPEG -i $f -preset slow -s $resolution -crf 24 $out 2> $OUTPUT_FOLDER/log.txt
+    $FFMPEG -i $f -nostdin -preset slow -s $resolution -crf 24 $out 2> $OUTPUT_FOLDER/log.txt
     if ! [[ $? -eq 0 ]]; then
-        echo "    ERROR!!! Check log.txt"
+        echo "  ERROR!!! Check log.txt"
         exit 1
     fi
+
+    # Get the new file bitrate and size
+    bitrate=$($FFMPEG -i $out -nostdin 2>&1 | grep -o "bitrate: \d*" | cut -b10-)
+    size=$(du -h $out | cut -f1)
+    echo "  NEW BITRATE    : $bitrate"
+    echo "  NEW SIZE       : $size"
+
+    # Copy the file timestamps from the original
     touch -r $f $out
-    echo "    Success!!"
+    echo "  Success!!"
     echo ""
 done
+
+echo Finished processing all files. Enjoy :P
